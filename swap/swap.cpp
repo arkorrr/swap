84d9dcec9b4b5a0778e967c02de7c4020806188e
trakt <trakt@DESKTOP-UDGVRCB>
2024-02-04 21:56:12 +0200

    Добавьте файлы проекта.

---
+++ swap/swap.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Swapper
+{
+
+public:
+	void Swap(T& a, T& b);
+};
+
+template<typename T>
+void Swapper<T>::Swap(T& a, T& b)
+{
+	T buffer = a;
+	a = b;
+	b = buffer;
+}
+
+
+int main()
+{
+	Swapper<int> swap;
+	int a = 5;
+	int b = 3;
+	swap.Swap(a,b);
+	cout << a << " " << b << endl;
+}
+
+
